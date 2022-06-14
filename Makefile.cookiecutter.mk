5dd6afab5827920b4aca734200e4b83339bb6be5
Thibault Ayanides <thibault.ayanides@skale-5.com>
2022-06-14 11:23:27 +0200

    fix: fix typo

--- Makefile.cookiecutter.mk
+++ Makefile.cookiecutter.mk
@@ -1,7 +1,7 @@
 # Copyright (C) 2022 Skale-5 <ops@skale-5.com>
 
 include makefiles/common/common.mk
-include makefiles/common/cookiecutter.mk
+include makefiles/cookiecutter/cookiecutter.mk
 include makefiles/custom/*.mk
 
 include makefiles/cookiecutter/dev.mk
