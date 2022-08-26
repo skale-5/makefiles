3f7877ba48752deb4ccc4561898c8c545b43a7b9
Thibault Ayanides <thibault.ayanides@skale-5.com>
2022-08-26 14:45:37 +0200

    feat(cicd-generator): add regen cicd generator
    closes #1, closes #3

--- gcp/dev.mk
+++ gcp/dev.mk
@@ -16,10 +16,16 @@ check: check-terraform check-kubectl check-kustomize check-conftest check-kubeva
 .PHONY: cookiecutter-create
 cookiecutter-create: ## Create a new environment using Cookiecutter
 	@echo -e "$(OK_COLOR)[$(BANNER)] Create environment$(NO_COLOR)"
-	@./sk5-scripts/cookiecutter-add-env.sh
+	@cookiecutter git@git.sk5.io:skale-5/cookiecutter/cookiecutter-gcp/cookiecutter-gcp-project.git --output-dir tmp/
 
 .PHONY: ci
 ci: ## Regen CI (use https://git.sk5.io/skale-5/gitlab-ci)
 	@rm -rf gitlab-ci/gitlab-ci && \
 	git clone git@git.sk5.io:skale-5/gitlab-ci.git gitlab-ci/gitlab-ci && \
 	rm -rf gitlab-ci/gitlab-ci/.git
+
+.PHONY: cicd-generator
+cicd-generator: ## Regen CI using CICD generator
+	@[ ! -d tmp/cicd-generator ] && git clone git@git.sk5.io:skale-5/cicd-generator.git tmp/cicd-generator || (cd tmp/cicd-generator && git pull && cd ../..) && \
+	cd tmp/cicd-generator && \
+	make builddocker VARS=./clients/$(CUSTOMER).yaml PROJECT=$(DIR) TAGS="debug,verbose"
