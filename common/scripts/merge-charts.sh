324c24aa87116117c5a61139add33cf9958e4dd8
Thibault AYANIDES <thibault.ayanides@skale-5.com>
2023-04-27 17:59:16 +0000

    feat: use only custom_merge if it is needed

--- common/scripts/merge-charts.sh
+++ common/scripts/merge-charts.sh
@@ -11,10 +11,28 @@ ENV=$2
 find_gnu=$(strings "$(which find)" | grep GNU)
 
 if [ -n "${find_gnu}" ]; then
-  files=$(find "${SERVICE}/values.yaml" "${SERVICE}/values/${ENV}" -name "*.yaml" -type f -printf "%p ")
+  files=$(find "${SERVICE}/values.yaml" "${SERVICE}/values/${ENV}" -maxdepth 1 -name "*.yaml" -type f -printf "%p ")
 else
   echo "On MAC find is not GNU find, you have to install findutils and put it in your path!"
   exit 1
 fi
-# shellcheck disable=SC2086
-yq ea '. as $item ireduce ({}; . *+ $item )' $files > ${FILE}
+
+
+custom_merge_vars=false
+
+# shellcheck disable=SC2206
+files_tab=($files)
+
+for f in "${files_tab[@]}"; do
+  if [[ $f != *values.yaml ]]; then
+    custom_merge_vars=true
+    break
+  fi
+done
+
+if $custom_merge_vars; then
+  # shellcheck disable=SC2086
+  yq ea '. as $item ireduce ({}; . *+ $item )' $files > ${FILE}
+else
+  cat "${SERVICE}/values/${ENV}/values.yaml" > ${FILE}
+fi
