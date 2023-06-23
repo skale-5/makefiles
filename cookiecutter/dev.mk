7a1f3d1d1ac8228ea51782b7f02ddb9c62d0af55
Thibault Ayanides <thibault.ayanides@skale-5.com>
2023-06-23 12:23:12 +0200

    feat: update make check

--- cookiecutter/dev.mk
+++ cookiecutter/dev.mk
@@ -5,10 +5,4 @@
 ##@ Development
 
 .PHONY: check
-check: check-cookiecutter check-terraform-docs check-pre-commit## Check requirements
-
-.PHONY: ci
-ci:
-	@rm -rf gitlab-ci/gitlab-ci && \
-	git clone git@git.sk5.io:skale-5/gitlab-ci.git gitlab-ci/gitlab-ci && \
-	rm -rf gitlab-ci/gitlab-ci/.git
+check: check-cookiecutter check-terraform-docs check-pre-commit check-helm## Check requirements
--- gcp/dev.mk
+++ gcp/dev.mk
@@ -10,11 +10,15 @@ clean: ## Cleanup
 	@find . -name "*.retry"|xargs rm -f
 
 .PHONY: check
-check: check-terraform check-kubectl check-kustomize check-conftest check-kubeval check-popeye check-tfsec check-tflint check-terraform-docs check-pre-commit check-yq
-## Check requirements
+check: check-terraform check-kubectl check-kustomize check-conftest check-kubeval check-popeye check-tfsec check-tflint check-terraform-docs check-pre-commit check-yq ## Check requirements
+	@echo
+	@echo BANNER=$(BANNER)
+	@echo CUSTOMER=$(CUSTOMER)
+	@echo BASTION_NAME=$(BASTION_NAME)
+	@echo EXPLOIT_ENV=$(EXPLOIT_ENV)
+	@echo BASTION_ZONE=$(BASTION_ZONE)
 
 .PHONY: cookiecutter-create
 cookiecutter-create: ## Create a new environment using Cookiecutter
 	@echo -e "$(OK_COLOR)[$(BANNER)] Create environment$(NO_COLOR)"
 	@cookiecutter git@git.sk5.io:skale-5/cookiecutter/cookiecutter-gcp/cookiecutter-gcp-project.git --output-dir tmp/
-
