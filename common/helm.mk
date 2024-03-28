76686cd2ca249f1e635c3d1828fb86c2e2495d09
Baptiste ROUX <baptiste.roux@skale-5.com>
2024-03-28 13:51:15 +0000

    BREAKING CHANGE: replace helm commands with helmfile

--- common/helm.mk
+++ common/helm.mk
@@ -5,68 +5,21 @@
 ##@ Helm
 
 .PHONY: helm-deps
-helm-deps: guard-SERVICE ## Install Helm chart dependencies (SERVICE=xxx)
-	@source $(SERVICE)/chart.sh && \
-		([ ! -f "$(SERVICE)/Chart.yaml" ] && \
-		helm repo add $$CHART_REPO_NAME $$CHART_REPO_URL && \
-		helm repo update) || \
-		([ -f "$(SERVICE)/Chart.yaml" ] && \
-		helm dependency update $(SERVICE) || \
-		echo "No dependencies found."); \
-
-
-.PHONY: helm-values
-helm-values: guard-SERVICE ## Show Helm values for the selected service (SERVICE=xxx)
-	@source $(SERVICE)/chart.sh && \
-		if [ ! -n "$(SKIP_DEPS)" ]; then \
-			echo -e "I will pull deps for you, next time you can use SKIP_DEPS=true "; \
-			([ ! -f "$(SERVICE)/Chart.yaml" ] && \
-			helm repo add $$CHART_REPO_NAME $$CHART_REPO_URL && \
-			helm repo update) || \
-			([ -f "$(SERVICE)/Chart.yaml" ] && \
-			helm dependency update $(SERVICE) || \
-			echo "No dependencies found."); \
-		fi; \
-		helm show values $${CHART_PATH:-$$CHART_REPO_NAME/$$CHART_NAME}
+helm-deps: guard-SERVICE guard-ENV ## Updating Helm chart dependencies (SERVICE=xxx ENV=xxx)
+	@helmfile deps -f $(SERVICE)/helmfile.yaml -e $(ENV)
 
 .PHONY: helm-template
 helm-template: guard-SERVICE guard-ENV ## Render chart templates locally and display the output. (SERVICE=xxx ENV=xxx)
-	@source $(SERVICE)/chart.sh && \
-		{ test -f $(SERVICE)/values/$(ENV)/chart.sh && source $(SERVICE)/values/$(ENV)/chart.sh || true; } && \
-		source makefiles/common/scripts/merge-charts.sh $(SERVICE) $(ENV) && \
-		helm template $${CHART_RELEASE_NAME:-$$CHART_NAME} $${CHART_PATH:-$$CHART_REPO_NAME/$$CHART_NAME} \
-		--namespace $$CHART_NAMESPACE -f $$FILE \
-		--version $$CHART_VERSION
-
-.PHONY: helm-validate
-helm-validate: guard-SERVICE guard-ENV kubernetes-check-context ## Simulate the installation/upgrade of a release (SERVICE=xxx ENV=xxx)
-	@source $(SERVICE)/chart.sh && \
-		{ test -f $(SERVICE)/values/$(ENV)/chart.sh && source $(SERVICE)/values/$(ENV)/chart.sh || true; } && \
-		source makefiles/common/scripts/merge-charts.sh $(SERVICE) $(ENV) && \
-		helm upgrade --install $${CHART_RELEASE_NAME:-$$CHART_NAME} $${CHART_PATH:-$$CHART_REPO_NAME/$$CHART_NAME} \
-		--namespace $$CHART_NAMESPACE -f $$FILE \
-		--version $$CHART_VERSION --create-namespace --dry-run
+	@helmfile template -f $(SERVICE)/helmfile.yaml -e $(ENV) --skip-deps
 
 .PHONY: helm-diff
 helm-diff: guard-SERVICE guard-ENV kubernetes-check-context ## Show diff of an installation/upgrade of the release (SERVICE=xxx ENV=xxx)
-	@source $(SERVICE)/chart.sh && \
-		{ test -f $(SERVICE)/values/$(ENV)/chart.sh && source $(SERVICE)/values/$(ENV)/chart.sh || true; } && \
-		source makefiles/common/scripts/merge-charts.sh $(SERVICE) $(ENV) && \
-		helm diff upgrade --install $${CHART_RELEASE_NAME:-$$CHART_NAME} $${CHART_PATH:-$$CHART_REPO_NAME/$$CHART_NAME} \
-		--namespace $$CHART_NAMESPACE -f $$FILE \
-		--version $$CHART_VERSION
+	@helmfile diff -f $(SERVICE)/helmfile.yaml -e $(ENV) --skip-deps
 
 .PHONY: helm-install
 helm-install: guard-SERVICE guard-ENV kubernetes-check-context ## Install/Upgrade the release (SERVICE=xxx ENV=xxx)
-	@source $(SERVICE)/chart.sh && \
-		{ test -f $(SERVICE)/values/$(ENV)/chart.sh && source $(SERVICE)/values/$(ENV)/chart.sh || true; } && \
-		source makefiles/common/scripts/merge-charts.sh $(SERVICE) $(ENV) && \
-		helm upgrade --install $${CHART_RELEASE_NAME:-$$CHART_NAME} $${CHART_PATH:-$$CHART_REPO_NAME/$$CHART_NAME} \
-		--namespace $$CHART_NAMESPACE -f $$FILE \
-		--version $$CHART_VERSION --create-namespace
+	@helmfile apply -f $(SERVICE)/helmfile.yaml -e $(ENV) --skip-deps
 
 .PHONY: helm-uninstall
 helm-uninstall: guard-SERVICE guard-ENV kubernetes-check-context ## Uninstall the release (SERVICE=xxx ENV=xxx)
-	@source $(SERVICE)/chart.sh && \
-		{ test -f $(SERVICE)/values/$(ENV)/chart.sh && source $(SERVICE)/values/$(ENV)/chart.sh || true; } && \
-		helm uninstall $${CHART_RELEASE_NAME:-$$CHART_NAME} --namespace $$CHART_NAMESPACE
+	@helmfile destroy -f $(SERVICE)/helmfile.yaml -e $(ENV) --skip-deps
